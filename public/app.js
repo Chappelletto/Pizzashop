a89392c03b10975edd23d3eb1dadc90c00717453
Peter M <sirchappelletto@rambler.ru>
2023-07-03 00:10:19 +0400

    updatr add_to_cart function

--- public/app.js
+++ public/app.js
@@ -12,8 +12,12 @@ function something()
 	// alert('hello');
 }
 
+
 function add_to_cart(id)
 {	
-	var t = 'You added pizza with id:' + id
-	alert(t)
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
